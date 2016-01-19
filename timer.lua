7167cd94f8b924f689364ba8af7ad85a43c21459
Erwin Harte <github@spam.is-here.com>
2016-01-19 13:54:39 -0600

    Trying to debug if there's any logic behind when the timer stops working.

--- timer.lua
+++ timer.lua
@@ -1,12 +1,19 @@
 local log = hs.logger.new("timer", "debug")
 log.i('Initializing')
 
+local counter = 0
 local tick = true
+local start_time = os.time()
+
+function timer_tick()
+    counter = counter + 1
+    log.df("T%sck .. #%d, going on %d seconds.", tick and "i" or "o", counter, os.time() - start_time)
+    tick = not tick
+end
 
 -- Reminder of time passing
-hs.timer.doEvery(300, function()
-    log.i("T"..(tick and "i" or "o").."ck .. ")
-    tick = not tick
-end)
+hs.timer.doEvery(300, timer_tick)
+
+timer_tick()
 
 log.i('Initialization finished.')
