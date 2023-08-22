67bee6da3f53a23cfe8741de84454deb950b0b39
Erwin Harte <github@spam.is-here.com>
2023-08-21 21:51:42 -0700

    Make the iTerm auto-placement code multi-screen aware
    Also ignore hidden or non-standard iTerm windows (preferences, update checks,
    that sort of things).

--- iterm.lua
+++ iterm.lua
@@ -1,57 +1,101 @@
+--[[
+	Automagically place the iTerm windows on a self-adjusting grid, as
+	windows are created, hidden, unhidden, or closed.
+
+	Still looking for a definitive way to sanely exclude any and all non-
+	terminal iTerm windows, though isStandard() seems to get close.
+]]
+
 if iterm then return end
 
 iterm = {}
 
--- XXX:
--- * Figure out a good way to not mess with the "Short Term Session Warning" modal dialog
--- * Apply the rules on a per-screen basis.
-
--- Automagically place the iTerm windows on a self-adjusting grid, as windows
--- are created and destroyed:
+iterm.log = hs.logger.new("iterm", "debug")
 
 local wf = hs.window.filter
 
--- It seems the app now calls itself iTerm2, not iTerm?
 iterm.filter = wf.new('iTerm2')
 
-iterm.adjust_grid = function (filter)
-	local list = filter:getWindows(wf.sortByCreated)
-	local total = 0
-	for i, w in ipairs(list) do
---		hs.alert.show("iTerm window "..tostring(w).." with desktop "..tostring(w.desktop()).."?", 5)
-		total = i
+iterm.analyze_windows = function (list, element)
+	local per_screen = {}
+	local seen = {}
+
+	if element then
+		table.insert(list, element)
 	end
-	if total < 1 then
+
+	for i, w in ipairs(list) do
+		if not seen[w] then
+			seen[w] = true
+			if w:isVisible() and w:isStandard() then
+				local sid = w:screen():id()
+				iterm.log.df("iTerm window %d: %s on screen %s", i, w:title(), sid)
+				local slist = {}
+				if not per_screen[sid] then
+					per_screen[sid] = {}
+				end
+				slist = per_screen[sid]
+				table.insert(slist, w)
+			end
+		end
+	end
+	return per_screen
+end
+
+iterm.adjust_grid = function (element, filter)
+	if element and element:title() == "Preferences" then
+		iterm.preference_window = element
 		return
 	end
-	local r = 1
-	local c = 1
-	while r * c < total do
-		r = r + 1
-		c = c + 1
+	if element and element == iterm.preference_window then
+		return
 	end
-	if total <= (r - 1) * c then
-		r = r - 1
-	end
-	-- hs.alert.show("Fit "..total.." window(s) into a "..r.."x"..c.." grid?")
-	local cur_r, cur_c
-	cur_r = 0
-	cur_c = 0
-	for i, w in ipairs(list) do
-		local rect = hs.geometry.new(cur_c / c, cur_r / r, 1 / c, 1 / r)
-		w:move(rect, nil, nil, 0)
-		cur_c = cur_c + 1
-		if cur_c == c then
+	local per_screen = { }
+
+	local seen = {}
+	local list = filter:getWindows(wf.sortByCreated)
+	per_screen = iterm.analyze_windows(list, element)
+
+	-- Loop over the screens that have iTerm windows on them:
+	for sid, list in pairs(per_screen) do
+		total = #list
+		-- hs.alert.show(total.." iTerm windows on "..sid)
+		if total >= 1 then
+			local r = 1
+			local c = 1
+			while r * c < total do
+				r = r + 1
+				c = c + 1
+			end
+			if total <= (r - 1) * c then
+				r = r - 1
+			end
+			-- hs.alert.show("Fit "..total.." window(s) into a "..r.."x"..c.." grid?")
+			local cur_r, cur_c
+			cur_r = 0
 			cur_c = 0
-			cur_r = cur_r + 1
+			for i, w in ipairs(list) do
+				local rect = hs.geometry.new(cur_c / c, cur_r / r, 1 / c, 1 / r)
+				w:move(rect, nil, nil, 0)
+				cur_c = cur_c + 1
+				if cur_c == c then
+					cur_c = 0
+					cur_r = cur_r + 1
+				end
+			end
 		end
 	end
 end
 
 iterm.filter:subscribe(wf.windowVisible, function (element, event)
-	iterm.adjust_grid(iterm.filter)
+	iterm.adjust_grid(element, iterm.filter)
 end)
 
 iterm.filter:subscribe(wf.windowNotVisible, function (element, event)
-	iterm.adjust_grid(iterm.filter)
+	iterm.adjust_grid(element, iterm.filter)
+end)
+
+-- Manually trigger the grid adjustment, no automatic adjustments:
+hs.hotkey.bind("control option command", "T", nil, function ()
+	iterm.adjust_grid(nil, iterm.filter)
 end)
