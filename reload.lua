2b1ab6e5d1fbf81c2bfc9bdff8195e58d3b1a349
Erwin Harte <github@spam.is-here.com>
2023-08-21 21:03:43 -0700

    Avoid triggering a reload on ".#*.lua" files (symlinks) made by Emacs
    Also add logging of what actually triggered the reload

--- reload.lua
+++ reload.lua
@@ -1,17 +1,23 @@
 if reload then return end
 
 reload = {}
+reload.log = hs.logger.new("reload", "debug")
 
 function reload.config(files)
-    doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-	    doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
+	doReload = false
+	for _, file in pairs(files) do
+		local basename = string.gsub(file, "(.*/)(.*)", "%2")
+		-- Ignore the ".../.#*" files
+		if basename:sub(1, 2) ~= ".#" and basename:sub(-4) == ".lua" then
+			doReload = true
+			reload.log.df("Configuration reload triggered by '%s'", basename)
+		else
+			reload.log.df("Ignoring change for '%s'", basename)
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
 end
 
 -- Set up path watcher:
